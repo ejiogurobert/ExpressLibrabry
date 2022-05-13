b28ecf737ee2832f1a6c3296aeb8d4250145c2f5
Robert Ejiogu <robertejiogu@gmail.com>
2022-05-13 18:01:54 +0100

    Retrieve data from product one

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,19 +1,52 @@
-// var storeModel = require("../models/storeModel")
-
+var storeModel = require("../models/storeModel")
 var allProducts = require("../db/inventories");
 
 exports.index = function( req, res, next) {
     // res.send("This is all we have in store")
     const allProducts = storeModel.index()
-
-    console.log(allProducts)
-    // res.send(allProducts)
+    
+    
+   
     res.render("allourproducts", { p :  allProducts})
 }
+
+
+//DELETING AN ITEM.
+
+exports.removeItem = function(req, res, next){
+    const { id } = req.params
+   
+    const allProducts = storeModel.index()
+   
+    // const isProductAvailable = allProducts.find(p => p.id === Number(id))
+   
+    // console.log(isProductAvailable);
+
+    // if(!isProductAvailable){
+
+    //     return res
+    //     .status(404)
+    //     .render({success: false, message: `There is no item with id: ${id}`})
+    // }
+    const remainingProducts = allProducts.filter(product => product.id !== Number(id))
+   
+    console.log(remainingProducts);
+    return res.status(200).render("remove", {success: true, product: remainingProducts})
+}
+
+
+exports.updateProducts = function (req, res, next) {
+    const priceIncrement = req.query.increment;
+    const allProducts = storeModel
+      .index()
+      .map((product) => product.price += Number(priceIncrement))
+  
+    res.send(allProducts);
+  };
+
+
 exports.getAnItem = function (req, res) {
     const id = req.params.id;
-
-    // const allProducts = storeModel.index();
     const newItem = allProducts.filter((product) => product.id === Number(id))
     return res.status(200).render('getitem', {success: true, item: newItem})
 }
